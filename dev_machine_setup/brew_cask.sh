fd25794a8feca9dbaec5bf4483d94cebcbb413be
Chris Jackson <crjackso@gmail.com>
2019-12-14 23:05:20 -0600

    Add initial Brewfile

--- dev_machine_setup/brew_cask.sh
+++ dev_machine_setup/brew_cask.sh
@@ -2,27 +2,21 @@
 
 sh ./brew_install_upgrade.sh
 
-# Install Caskroom
-brew tap caskroom/cask
-brew install brew-cask
-brew tap caskroom/versions
+brew tap homebrew/cask-cask
+brew tap homebrew/cask-versions
 
 # Install packages
 apps=(
     alfred
     1password
     dropbox
-    google-drive
+    google-drive-file-stream
     flux
     iterm2
-    atom
     visual-studio-code
     google-chrome
     google-chrome-canary
-    malwarebytes-anti-malware
     nordvpn
-    glimmerblocker
-    macdown
     spotify
     slack
     transmission
